cc375ae54dd98bdf79a8d2601ce95d39085855de
flying-colors <move.on.fight@gmail.com>
2019-06-09 00:16:53 +0900

    Update s_pamph_confirm.js

--- s_pamph_confirm.js
+++ s_pamph_confirm.js
@@ -1,50 +1,63 @@
-var $ = require('jquery');
-
 $(function () {
     // カウントダウン文言
     class Popup {
         constructor() {
-            this.countAddSchool = 0;
+            this.countRemainNum = $('.js-countDown').data('remain-num');
+            this.allShiryoCdList = []; // 全資料コードリスト
             this.init();
         }
+        // 初期化
         init() {
             this.setPopup();
-            this.getRemainNum();
+            this.pushAddedShiryoCd();
+            this.pushInputShiryoCd();
+            this.calcRemainNum();
+            this.updateRemainNum();
             this.bindEvent();
         }
+        // カウントダウン文言を表示
         setPopup(){
             if($('.output').length){
                 $('.popup').show().insertAfter('.output:last');
             }
         };
-        getRemainNum() {
-            this.countRemainNum = 7;
-
-            const arrShiryoCd = [];
-
-            addedShiryoCd = $('[name="addedShiryoCd"]').val().split(',');
-
-            console.log(addedShiryoCd);
-            
+        // 前画面で入力済みの資料コードをリストに追加
+        pushAddedShiryoCd() {
+            const addedShiryoCd = $('[name="addedShiryoCd"]').val();
+            const arrAddedShiryoCd = addedShiryoCd !== ""  ? addedShiryoCd.split(',')  : '';
+            if(addedShiryoCd !== ''){
+                arrAddedShiryoCd.forEach((value)=>{
+                    this.allShiryoCdList.push(value);
+                });
+            }
+        }
+        // 入力した資料コードをリストに追加
+        pushInputShiryoCd() {
             $('[data-shiryocd]').each((index,elm)=>{
                 const shiryoCd = $(elm).data('shiryocd').toString();
+                // 有料校を除外
                 if($(elm).data('yuryoflag') === 0){
-                    arrShiryoCd.push(shiryoCd);
+                    this.allShiryoCdList.push(shiryoCd);
                 }
-                arrShiryoCd.push(addedShiryoCd);
             });
-                        
-            const setShiryoCd = new Set(arrShiryoCd);
-            console.log(setShiryoCd);
-            
+        }
+        // プレゼントに必要な応募校数を計算
+        calcRemainNum() {
+            // 全資料コードリストの重複を削除
+            const setShiryoCd = new Set(this.allShiryoCdList);
+            this.countRemainNum -= setShiryoCd.size;
+        }
+        // プレゼントに必要な応募校数を表示
+        updateRemainNum() {
             if(this.countRemainNum > 0){
-                $('.js-countDown').text(this.countRemainNum - setShiryoCd.size);
+                $('.js-countDown').text(this.countRemainNum);
             } else {
                 $('.js-countDown').text('0');
                 $('.popup').addClass('is-full');
                 return false;
             }
         };
+        // イベントをバインド
         bindEvent(){
             $('.js-addSchool').on('click',()=>{
                 if($('.input').val() === ''){
